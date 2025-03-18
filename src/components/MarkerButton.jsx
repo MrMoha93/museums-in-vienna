a2489a239c8226d9db78b2152cf2f7de2ddb4a32
MohaK <mohakhalili@outlook.com>
2025-03-18 19:07:38 +0100

    feat: image folder and updated marker button with image

--- src/components/MarkerButton.jsx
+++ src/components/MarkerButton.jsx
@@ -1,10 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLandmark } from "@fortawesome/free-solid-svg-icons";
-
 const MarkerButton = ({ place, onSelect }) => {
   return (
     <button className="marker-btn" onClick={(e) => onSelect(e, place)}>
-      <FontAwesomeIcon icon={faLandmark} size="2x" />
+      <img src="/images/museum.png" alt="museum" className="marker-img" />
     </button>
   );
 };
